0f8fc9b4e74c36e86218f9d660ef41ada65fac41
Amin Shariati <amin.shariati@accenture.com>
2017-06-30 04:30:42 +0800

    Code cleanup

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ module.exports = function (inputString, options) {
   inputString = (inputString || '112233445566778899').trim();
   options = options || {};
 
-  let MASKED_CHARACTER = '#';
-  let TOTAL_UNMASKED_TRAILING_CHARACTERS = 0;
-  let TOTAL_UNMASKED_LEADING_CHARACTERS = 0;
+  const MASKED_CHARACTER = '#';
+  const TOTAL_UNMASKED_TRAILING_CHARACTERS = 0;
+  const TOTAL_UNMASKED_LEADING_CHARACTERS = 0;
 
   let maskedCharacter = MASKED_CHARACTER;
   let totalUnmaskedTrailingCharacters = TOTAL_UNMASKED_TRAILING_CHARACTERS;
@@ -17,34 +17,31 @@ module.exports = function (inputString, options) {
     if (maskedCharacter.length > 1) {
       maskedCharacter = options.maskedCharacter.charAt(0);
     }
-    MASKED_CHARACTER = maskedCharacter;
   }
 
   if (options.totalUnmaskedTrailingCharacters) {
     totalUnmaskedTrailingCharacters = Math.abs(options.totalUnmaskedTrailingCharacters);
-    TOTAL_UNMASKED_TRAILING_CHARACTERS = totalUnmaskedTrailingCharacters;
   }
 
   if (options.totalUnmaskedLeadingCharacters) {
     totalUnmaskedLeadingCharacters = Math.abs(options.totalUnmaskedLeadingCharacters);
-    TOTAL_UNMASKED_LEADING_CHARACTERS = totalUnmaskedLeadingCharacters;
   }
 
   let totalLenght = inputString.length;
   let maskedLength;
   let maskedBuffer = '';
 
-  maskedBuffer = maskedBuffer.concat(inputString.substring(0, TOTAL_UNMASKED_LEADING_CHARACTERS));
-  if (totalLenght > TOTAL_UNMASKED_TRAILING_CHARACTERS + TOTAL_UNMASKED_LEADING_CHARACTERS) {
-    maskedLength = totalLenght - (TOTAL_UNMASKED_TRAILING_CHARACTERS + TOTAL_UNMASKED_LEADING_CHARACTERS);
+  maskedBuffer = maskedBuffer.concat(inputString.substring(0, totalUnmaskedLeadingCharacters));
+  if (totalLenght > totalUnmaskedTrailingCharacters + totalUnmaskedLeadingCharacters) {
+    maskedLength = totalLenght - (totalUnmaskedTrailingCharacters + totalUnmaskedLeadingCharacters);
     for (let i = 0; i < maskedLength; i++) {
-      maskedBuffer += MASKED_CHARACTER;
+      maskedBuffer += maskedCharacter;
     }
   } else {
     maskedLength = 0;
-    totalLenght = TOTAL_UNMASKED_TRAILING_CHARACTERS + TOTAL_UNMASKED_LEADING_CHARACTERS;
+    totalLenght = totalUnmaskedTrailingCharacters + totalUnmaskedLeadingCharacters;
   }
 
-  maskedBuffer = maskedBuffer.concat(inputString.substring(TOTAL_UNMASKED_LEADING_CHARACTERS + maskedLength, totalLenght));
+  maskedBuffer = maskedBuffer.concat(inputString.substring(totalUnmaskedLeadingCharacters + maskedLength, totalLenght));
   return maskedBuffer.toString();
 };
